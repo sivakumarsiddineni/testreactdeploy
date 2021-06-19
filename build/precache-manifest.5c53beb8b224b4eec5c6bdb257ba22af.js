a76478ce8ca2c0e5c399219661c74e893b9ee0b3
Sivakumar Siddineni <0004963@trulytravel.tech>
2021-06-19 05:27:06 +0100

    siva code

---
+++ build/precache-manifest.5c53beb8b224b4eec5c6bdb257ba22af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705d7ee5c053dedb9c7e5a35654d5256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78dbecf76ecf9720cf7a",
+    "url": "/static/css/main.07b658fc.chunk.css"
+  },
+  {
+    "revision": "a71083aa3704dc0fe8c1",
+    "url": "/static/js/2.d46e77f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d46e77f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78dbecf76ecf9720cf7a",
+    "url": "/static/js/main.c89c5940.chunk.js"
+  },
+  {
+    "revision": "fbaab9a582e8e68a6c8c",
+    "url": "/static/js/runtime-main.2ae115fb.js"
+  }
+]);
